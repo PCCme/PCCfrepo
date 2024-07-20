d854f14fc2c5a23e3aa941d92dce52d64f3e04ff
mimi tianfu <mimitainfu@newbie.com>
2024-07-19 16:10:28 -0800

    Version1

---
+++ GitLearn/Do.js
@@ -0,0 +1,6 @@
+
+
+console.log( " I want to be a computer whizkid and a worldclass amazing writer");
+
+
+console.log( " Am gonna be a 100*  swe");
---
+++ Show.js
@@ -0,0 +1,6 @@
+
+
+console.log( " Just look at me build cool shit");
+
+
+console.log( "  I will be known and sought across the world for building amazing projects");
