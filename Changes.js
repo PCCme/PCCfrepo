84ed3da18a55ec1b360c1c16778812bbc5b6d7ba
mimi tianfu <mimitainfu@newbie.com>
2024-07-20 04:49:39 -0800

    i added some line of code to the Changes.js on the see branch

--- Changes.js
+++ Changes.js
@@ -4,3 +4,9 @@ console.log( " Just look at me build cool shit");
 
 
 console.log( "  I will be known and sought across the world for building amazing projects");
+
+
+console.log( " Problem solving is a great skill");
+
+console.log( " I need to have strong mathematical skills");
+
--- GitLearn/Track.js
+++ GitLearn/Track.js
@@ -1,3 +1,8 @@
 
 
 console.log( " Just look at me build cool shit");
+
+
+console.log( " Building stuffs makes me excited");
+
+
